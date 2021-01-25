c50ea8b56afe105f9e899a0a9f15ddd8fcd180aa
agnorance <david.mihajlovic@protonmail.com>
2021-01-25 17:24:08 +0100

    add newton verfahren

---
+++ NewtonVerfahren.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def newton(f, Df, x0, epsilon, max_iter):
+    """Approximate solution of f(x)=0 by Newton's method.
+
+    Parameters
+    ----------
+    f : function
+        Function for which we are searching for a solution f(x)=0.
+    Df : function
+        Derivative of f(x).
+    x0 : number
+        Initial guess for a solution f(x)=0.
+    epsilon : number
+        Stopping criteria is abs(f(x)) < epsilon.
+    max_iter : integer
+        Maximum number of iterations of Newton's method.
+
+    Returns
+    -------
+    xn : number
+        Implement Newton's method: compute the linear approximation
+        of f(x) at xn and find x intercept by the formula
+            x = xn - f(xn)/Df(xn)
+        Continue until abs(f(xn)) < epsilon and return xn.
+        If Df(xn) == 0, return None. If the number of iterations
+        exceeds max_iter, then return None.
+    """
+    xn = x0
+    for n in range(0, max_iter):
+        fxn = f(xn)
+        if abs(fxn) < epsilon:
+            print('Found solution after', n, 'iterations.')
+            return xn
+        Dfxn = Df(xn)
+        if Dfxn == 0:
+            print('Zero derivative. No solution found.')
+            return None
+        xn = xn - fxn / Dfxn
+        # print(xn) uncomment to see iterations
+    print('Exceeded maximum iterations. No solution found.')
+    return None
+
+
+# Funktion
+def f(x):
+    return (np.e ** (x ** 2)) + (x ** -3) - 10
+
+
+# Ableitung
+def Df(x):
+    return 2 * x * (np.e ** (x ** 2)) - (3 / (x ** 4))
+
+
+print(newton(f, Df, 2, 1e-8, 10))
